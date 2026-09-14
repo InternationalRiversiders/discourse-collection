88218c978096c447a1ffae5d04b6ef73699c40e6
0x444858 <mail.denghanxu@gmail.com>
2026-09-14 21:50:22 +0800

    release 1.2.0

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-collection
 # about: Public collections: signed-in users create topic collections, co-maintain them, feature selected replies, and subscribe to updates.
-# version: 1.1.0
+# version: 1.2.0
 # authors: 0x444858
 
 enabled_site_setting :collection_enabled
